aab3ea1cd169ab695ac34a05267e9dbcc4cb0352
Mahdi <44380968+mbonyani@users.noreply.github.com>
2022-06-23 19:20:21 +0430

    Update generate_training_data.py

--- stgcn_traffic_prediction/dataloader/generate_training_data.py
+++ stgcn_traffic_prediction/dataloader/generate_training_data.py
@@ -96,3 +96,23 @@ def generate_train_val_test(args):
             x_offsets=x_offsets.reshape(list(x_offsets.shape) + [1]),
             y_offsets=y_offsets.reshape(list(y_offsets.shape) + [1]),
         )
+
+
+def main(args):
+    print("Generating training data")
+    generate_train_val_test(args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output_dir", type=str, default="data/", help="Output directory."
+    )
+    parser.add_argument(
+        "--traffic_df_filename",
+        type=str,
+        default="data/metr-la.h5",
+        help="Raw traffic readings.",
+    )
+    args = parser.parse_args()
+    main(args)
